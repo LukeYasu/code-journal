48f67c0947d4fce4de0adfbf8939e8d0d3086c1e
LukeYasu <lucasyasumura@gmail.com>
2024-09-17 16:58:17 +0000

    code journal layout revised #1

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
+'use strict';
 // const $entryTitle = document.querySelector('#entry-title');
 // const $photoURL = document.querySelector('#photo-url');
 // if (!$entryTitle) throw new Error('$entryTitle query failed');
 // if (!$photoURL) throw new Error('$photoURL query failed');
-
 // function handleKeydown(event: Event): void {
 //   x;
 // }
-
 // $entryTitle.addEventListener('keydown', x);
-
-// const $saveButton = document.querySelector('.save-button');
-// if (!$saveButton) throw new Error('$saveButton query failed');
+const $saveButton = document.querySelector('.save-button');
+if (!$saveButton) throw new Error('$saveButton query failed');
