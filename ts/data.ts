d24051f36dce0f470a1ca89b19f72164ba72c56a
LukeYasu <lucasyasumura@gmail.com>
2024-09-17 16:45:34 +0000

    code journal layout revised #1

--- ts/data.ts
+++ ts/data.ts
@@ -4,3 +4,9 @@ const data = {
   editing: null,
   nextEntryId: 1,
 };
+
+const $entryTitle = document.querySelector('#entry-title');
+const $photoURL = document.querySelector('#photo-url');
+if (!$entryTitle) throw new Error('$entryTitle query failed');
+if (!$photoURL) throw new Error('$photoURL query failed');
+console.log('aaaaaa');
