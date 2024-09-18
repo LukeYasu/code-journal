27084ae1044aa8d2354af710a4dc29de8a058e39
LukeYasu <lucasyasumura@gmail.com>
2024-09-18 16:25:56 +0000

    code journal issue #1 complete

--- js/main.js
+++ js/main.js
@@ -4,36 +4,32 @@ const $entryTitle = document.querySelector('#entry-title');
 const $photoURL = document.querySelector('#photo-url');
 const $entryNotes = document.querySelector('#entry-notes');
 const $img = document.querySelector('img');
-const $saveButton = document.querySelector('.save-button');
 const $form = document.querySelector('form');
 if (!$entryTitle) throw new Error('$entryTitle query failed');
 if (!$photoURL) throw new Error('$photoURL query failed');
 if (!$entryNotes) throw new Error('$entryNotes query failed');
 if (!$img) throw new Error('$img query failed');
-if (!$saveButton) throw new Error('$saveButton query failed');
 if (!$form) throw new Error('$form query failed');
-$saveButton.addEventListener('click', handleClick);
+$form.addEventListener('submit', handleSubmit);
 $photoURL.addEventListener('input', handleInput);
 function handleInput() {
   const photoSRC = $photoURL.value;
   $img?.setAttribute('src', photoSRC);
 }
 let nextEntryIdNum = data.nextEntryId;
-function handleClick(event) {
+function handleSubmit(event) {
   event.preventDefault();
   const eventTarget = event.target;
-  if (eventTarget.matches('.button')) {
-    const newFormEntry = {
-      entryId: nextEntryIdNum,
-      nextEntryId: nextEntryIdNum + 1,
-      title: $entryTitle.value,
-      photoURL: $photoURL.value,
-      notes: $entryNotes.value,
-    };
-    nextEntryIdNum = newFormEntry.entryId + 1;
-    data.entries.unshift(newFormEntry);
-    $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
-    $form?.reset();
-    writeData();
-  }
+  const newFormEntry = {
+    entryId: nextEntryIdNum,
+    nextEntryId: nextEntryIdNum + 1,
+    title: $entryTitle.value,
+    photoURL: $photoURL.value,
+    notes: $entryNotes.value,
+  };
+  nextEntryIdNum = newFormEntry.entryId + 1;
+  data.entries.unshift(newFormEntry);
+  $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form?.reset();
+  writeData();
 }
--- ts/main.ts
+++ ts/main.ts
@@ -3,16 +3,14 @@ const $entryTitle = document.querySelector('#entry-title') as HTMLFormElement;
 const $photoURL = document.querySelector('#photo-url') as HTMLFormElement;
 const $entryNotes = document.querySelector('#entry-notes') as HTMLFormElement;
 const $img = document.querySelector('img');
-const $saveButton = document.querySelector('.save-button');
-const $form = document.querySelector('form');
+const $form = document.querySelector('form') as HTMLFormElement;
 if (!$entryTitle) throw new Error('$entryTitle query failed');
 if (!$photoURL) throw new Error('$photoURL query failed');
 if (!$entryNotes) throw new Error('$entryNotes query failed');
 if (!$img) throw new Error('$img query failed');
-if (!$saveButton) throw new Error('$saveButton query failed');
 if (!$form) throw new Error('$form query failed');
 
-$saveButton.addEventListener('click', handleClick);
+$form.addEventListener('submit', handleSubmit);
 $photoURL.addEventListener('input', handleInput);
 
 function handleInput(): void {
@@ -30,23 +28,19 @@ interface codeJournalForm {
 
 let nextEntryIdNum = data.nextEntryId;
 
-function handleClick(event: Event): void {
+function handleSubmit(event: SubmitEvent): void {
   event.preventDefault();
+  const newFormEntry: codeJournalForm = {
+    entryId: nextEntryIdNum,
+    nextEntryId: nextEntryIdNum + 1,
+    title: $entryTitle.value,
+    photoURL: $photoURL.value,
+    notes: $entryNotes.value,
+  };
+  nextEntryIdNum = newFormEntry.entryId + 1;
+  data.entries.unshift(newFormEntry);
 
-  const eventTarget = event.target as HTMLElement;
-  if (eventTarget.matches('.button')) {
-    const newFormEntry: codeJournalForm = {
-      entryId: nextEntryIdNum,
-      nextEntryId: nextEntryIdNum + 1,
-      title: $entryTitle.value,
-      photoURL: $photoURL.value,
-      notes: $entryNotes.value,
-    };
-    nextEntryIdNum = newFormEntry.entryId + 1;
-    data.entries.unshift(newFormEntry);
-
-    $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
-    $form?.reset();
-    writeData();
-  }
+  $img?.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form?.reset();
+  writeData();
 }
